04a7f0dba84a5ce33725580f8d3a7619398506ca
BrockCarlson-Lenovo <brockmcarlson@gmail.com>
2022-05-03 11:38:45 -0500

    end of Deep Work 1 - May 3
    next step - create IDX

--- Controller/visWithGramm_IOT.m
+++ Controller/visWithGramm_IOT.m
@@ -1,2 +1,46 @@
-% visWithGramm_IOT()
+%% visWithGramm_IOT
+% This Github release is at the controller level and is accessing 
+% sub-routines in the Viewer level. This controller interface is intended 
+% to create Gramm plots of the standard IOT comparison. Standard IOT 
+% (aka stdIOTcomp) is simply the monocular presentation of one eye compared 
+% to the same presentation after adaptation in the other eye. 
+
+% This controller interface is intended to take in the the SDF and STIM 
+% variable outputs created in the pre-processing step. It will then format 
+% them for proper gramm inputs. Finally, the controller will interact with
+% the View directory to format and plot the figures using Gramm.
+
+%% Manual Inputs 
+clear
+close all
+
+sessionOfInterest = '211008_B_bmcBRFS001';
+
+%% Load in Data
+setup_IOT('BrockWork')
+global OUTDIR_FD
+cd(OUTDIR_FD)
+formattedFileName = strcat(sessionOfInterest,'_FD.mat');
+load(formattedFileName)
+
+
+
+%% obtainConditionsOfInterest()
+% The goal of this file is to generate an IDX output
+IDX = obtainConditionsOfInterest(trialAlignedMUAPacket);
+
+
+
+
+
+%% formatForGrammInput
+
+
+
+
+
+%% plotStdIOTwithGramm
+
+
+
 
---
+++ View/obtainConditionsOfInterest.m
@@ -0,0 +1,20 @@
+%% obtainConditionsOfInterest
+%The goal of this function is to create a standard IDX output
+
+%%
+function IDX = obtainConditionsOfInterest(trialAlignedMUAPacket)
+
+
+
+
+
+
+
+
+
+
+
+
+
+IDX = [];
+end
